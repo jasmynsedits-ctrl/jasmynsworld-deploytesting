11c47c205ca25a218c85e7a4a3a89b6ac9b91470
jasmynsedits-ctrl <jasmynsedits@gmail.com>
2026-06-08 18:21:52 -0700

    Update MyRoom.tsx

--- artifacts/3d-game/src/components/rooms/MyRoom.tsx
+++ artifacts/3d-game/src/components/rooms/MyRoom.tsx
@@ -12,7 +12,7 @@ const TOYS = [
 
 const MyRoom = () => {
   const [selectedToy, setSelectedToy] = useState(null);
-  const bgUrl = "https://photos.google.com/share/AF1QipO-WGEWtpSHVD4TmGkTzb52jyYUQ0fieGC46aaJoMUiGX1jYfV4asyoLp7FD_RYBg/photo/AF1QipOFKWsIen1IIp1dFIAEVPRFa8fNY5fA-a5WyW3K?key=RGtfSUJYdzgyQ1RodWVVSnpJVUZHYTNKQkhBdHJR";
+  const bgUrl = "https://lh3.googleusercontent.com/pw/AP1GczOKuj3oJ2SzDmpzjJlp5lfZhli0nlWcYb-KibxvMCXJpcxbDIKDRezIQeP6OPWsg5mNNk7JSV8eeqt41irWF3coGOpXURFdAfsjLIfOC5LWS9zSOyC9u9VnyQbR8c_y30k2awYBqoSDsqUJZfMbK8REuw=w1035-h947-s-no-gm?authuser=0";
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#1a1c2c] p-4 font-mono text-white">
